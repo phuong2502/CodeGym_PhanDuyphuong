9a1e74f026c10e3c71b37bf7f66e70456c157bde
ADMIN <phuong2502>
2019-10-14 17:05:42 +0700

    s

---
+++ Module1/BienKieuDuLieuVaToanTu/ThucHanhSuDungToanTu/ToanTu1.js
@@ -0,0 +1,8 @@
+let inputWidth = 10 ;
+let inputHeight = 20;
+inputWifth = prompt("Enter the Width = 10")
+inputHeight = prompt("Enter the Height = 20")
+let Width = parseInt(inputWidth = 10);
+let Height = parseInt(inputHeight = 20);
+let area = Width * Height;
+document.write("the area is: " + area);
