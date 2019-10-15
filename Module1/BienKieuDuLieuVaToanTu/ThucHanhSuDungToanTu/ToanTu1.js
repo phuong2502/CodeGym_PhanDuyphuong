fae7f08af77d39b03dfa35f8e8a7e32600374c80
ADMIN <phuong2502>
2019-10-15 16:26:40 +0700

    s

--- Module1/BienKieuDuLieuVaToanTu/ThucHanhSuDungToanTu/ToanTu1.js
+++ Module1/BienKieuDuLieuVaToanTu/ThucHanhSuDungToanTu/ToanTu1.js
@@ -1,8 +1,8 @@
-let inputWidth = 10 ;
+let inputWidth = 10;
 let inputHeight = 20;
-inputWifth = prompt("Enter the Width = 10")
-inputHeight = prompt("Enter the Height = 20")
+inputWifth = prompt("Enter the Width")
+inputHeight = prompt("Enter the Height")
 let Width = parseInt(inputWidth = 10);
-let Height = parseInt(inputHeight = 20);
+let Height = parseInt(inputHeigh = 20);
 let area = Width * Height;
 document.write("the area is: " + area);
